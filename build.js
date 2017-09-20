211816d3090f96590b5321b06cedff54b395d79e
Todd Dukart <tdukart@aloftinteractive.com>
2017-09-19 23:39:33 -0400

    Switch back to multi-file

--- build.js
+++ build.js
@@ -2,7 +2,7 @@
 
 var fs = require( 'fs' );
 var wordListPath = require( 'word-list' );
-var json5 = require('json5');
+var JSON5 = require( 'json5' );
 
 var disassembleString = function ( string ) {
 	var filteredString = string.replace( /[^A-Z]/gi, '' ).toUpperCase();
@@ -16,10 +16,12 @@ var disassembleString = function ( string ) {
 	return components;
 };
 
-var deconstructedWords = [];
+var deconstructedWords = {};
 
 fs.readFileSync( wordListPath, 'utf8' ).split( '\n' ).forEach( function ( word ) {
-	deconstructedWords.push( [ word, disassembleString( word ) ] );
+	var length = word.length;
+	deconstructedWords[ length ] = deconstructedWords[ length ] || [];
+	deconstructedWords[ length ].push( [ word, disassembleString( word ) ] );
 } );
 
 if ( !fs.existsSync( 'dist' ) ) {
@@ -27,7 +29,42 @@ if ( !fs.existsSync( 'dist' ) ) {
 }
 
 var indexScript = [];
+var upToLengthScripts = {};
 
-indexScript.push( 'module.exports = ' + json5.stringify( deconstructedWords ) + ';' );
+indexScript.push( 'var words = [];' );
+
+var lengths = Object.keys( deconstructedWords ).map( function ( length ) {
+	return parseInt( length, 10 );
+} );
+
+var maxLength = Math.max.apply(null,lengths);
+
+for ( var i = 2; i <= maxLength; i++ ) {
+	upToLengthScripts[ i ] = [ 'var words = [];' ];
+}
+
+lengths.forEach( function ( length ) {
+	var lengthScript = 'module.exports = ' + JSON5.stringify( deconstructedWords[ length ] );
+
+	var fileName = 'length-' + length + '.js';
+
+	fs.writeFileSync( 'dist/' + fileName, lengthScript );
+
+	var concatCommand = 'words = words.concat( require( "./' + fileName + '" ) );';
+
+	indexScript.push( concatCommand );
+	for ( var i = length; i <= maxLength; i++ ) {
+		upToLengthScripts[ i ].push( concatCommand );
+	}
+
+} );
+
+indexScript.push( 'module.exports = words;' );
 
 fs.writeFileSync( 'dist/index.js', indexScript.join( '\n' ) );
+
+Object.keys( upToLengthScripts ).forEach( function ( length ) {
+	var script = upToLengthScripts[ length ].join( '\n' );
+	script += '\nmodule.exports = words;';
+	fs.writeFileSync( 'dist/up-to-' + length + '.js', script );
+} );
